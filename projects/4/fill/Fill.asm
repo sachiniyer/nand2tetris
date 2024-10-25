cad9fc24dc2a137d198dce7a1e53693cd39adee6
Sachin Iyer <sachinjiyer@gmail.com>
2024-10-25 00:09:20 -0700

    add project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,44 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+
+(BEGIN)
+    @24576
+    D=A
+    @input // Set the address of the input (24579) to @input
+    M=D
+    @16384
+    D=A
+    @SCREEN
+    M=D // Set the address of the screen (16384) to @SCREEN
+(CHECK_INPUT)
+    @24575
+    D=A
+    @oldvalue
+    M=D // set the last address of the screen to @oldvalue
+    @input
+    A=M
+    D=M // gather the value of the input into D
+    @newvalue
+    M=-1 // in binary, -1 is all 1's, which is black
+    @DRAW
+    D;JNE // Jump if @input is not 0 (screen will be black because @newvalue is -1)
+    @newvalue
+    M=0 // (screen will be white because @newvalue is 0)
+(DRAW)
+    @newvalue
+    D=M // Get the value of @newvalue into D
+    @oldvalue
+    A=M
+    M=D // Set the value of @newvalue to @oldvalue
+    @oldvalue
+    D=M // Get the value of @oldvalue into D
+    @SCREEN
+    D=D-A // Subtract the value of @oldvalue from
+    @CHECK_INPUT
+    D;JLE // Compare D to A (D=@oldvalue, A=@SCREEN)
+    @oldvalue
+    M=M-1
+    @DRAW
+    0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,22 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Adds 1+...+100.
+@R2
+    M=0 // Set R2=0
+(LOOP)
+    @R1
+    D=M // D=R1
+    @END
+    D;JEQ // if R1 is 0 goto END
+    @R0
+    D=M // D=R2
+    @R2
+    M=D+M // add R0 to sum
+    @R1
+    M=M-1 // decrement R1
+    @LOOP
+    0;JMP // Goto LOOP
+(END)
+    @END
+    0;JMP // Infinite loop at end
